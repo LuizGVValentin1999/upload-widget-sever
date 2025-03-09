bd2e6ad595ab8775280ded3a2f840e582387babd
Luiz Gustavo Valter Valentin <luiz.gustavo1999@hotmail.com>
2025-03-09 17:52:33 -0300

    teste de commit

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -3,7 +3,7 @@ import { fastify } from 'fastify'
 
 const server = fastify()
 
-server.register(fastifyCors, { origin: '*' })
+server.register(fastifyCors, { origin: '*' })// teste de commit 
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP Server running!')
